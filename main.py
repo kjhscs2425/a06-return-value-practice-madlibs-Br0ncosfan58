4fa17fed8f192d930c784a371d7281bda8138cb2
Br0ncosfan58 <mwasserstein2025@kehillahstudent.org>
2024-11-10 11:54:47 -0800

    a06 exercise

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-adjective0 = input("Give me an adjective: ")
-noun0 = input("Give me a noun: ")
+adjective1 = input("Give me an adjective:")
+noun1 = input("Give me a noun:")
+pluralnoun1 = input("Give me a plural noun:")
+personinroomfemale0 = input("Give me a person in room (female):")
+adjective2 = input("Give me an adjective:")
+articleofclothing0 = input("Give me an article of clothing:")
+noun2 = input("Give me a noun:")
+city0 = input("Give me a city:")
+pluralnoun2 = input("Give me a plural noun:")
+adjective3 = input("Give me another adjective:")
 
-story = f"There are many {adjective0} ways to choose a/n {noun0} to read."
+story = f"There are many {adjective1} ways to choose a/n {noun1} to read. First, you could ask for recommendations from your friends and {pluralnoun1}. Just don't ask Aunt {personinroomfemale0} --she only reads {adjective2} books with {articleofclothing0} -ripping goddesses on the cover. If your friends and family are no help, try checking out the {noun2} Review in The {city0} Times. If the {pluralnoun2} featured there are too {adjective3} for your taste, try something a little."
 print(story)
